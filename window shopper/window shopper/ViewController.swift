15d0268b6f00fcfbc8e3db22d537b00d1b845659
Ghoul Mehdi <ghoulmehdi@MacBook-Pro-de-Ghoul.local>
2018-06-25 16:54:36 +0100

    first commit

---
+++ window shopper/window shopper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  window shopper
+//
+//  Created by Ghoul Mehdi on 25/06/2018.
+//  Copyright © 2018 Ghoul Mehdi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
