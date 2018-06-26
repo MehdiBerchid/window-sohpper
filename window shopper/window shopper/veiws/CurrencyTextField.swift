f446ab867b4870f1c78264943d566fa1e45991f0
Ghoul Mehdi <ghoulmehdi@MacBook-Pro-de-Ghoul.local>
2018-06-26 02:41:58 +0100

    custumazie ui/ux

--- window shopper/window shopper/veiws/CurrencyTextField.swift
+++ window shopper/window shopper/veiws/CurrencyTextField.swift
@@ -7,23 +7,28 @@
 //
 
 import UIKit
+@IBDesignable
 
 class CurrencyTextField: UITextField {
+    override func prepareForInterfaceBuilder() {
+        CustomizeView()
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
-        layer.cornerRadius = 5.00
-        textAlignment = .center
-      
-        if let p = placeholder {
-            let place = NSAttributedString(string: p , attributes: [ .foregroundColor : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
-            attributedPlaceholder = place
-            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-            
-            
+        CustomizeView()
         }
-        
+    
+    func CustomizeView() {
+            backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+            layer.cornerRadius = 5.00
+            textAlignment = .center
+            
+            if let p = placeholder {
+                let place = NSAttributedString(string: p , attributes: [ .foregroundColor : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+                attributedPlaceholder = place
+                textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
     }
     
 
